c8920a12d79110de64c471de61fc6d3f64047808
Joselu <joseluissgz97@gmail.com>
2024-07-06 22:57:34 +0200

    feat: Add dotenv configuration for environment variables

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,4 @@
+import 'dotenv/config';
 import express, { NextFunction, Request, Response } from 'express';
 import { usersRouter } from './routes/user.routes';
 import { ErrorClient } from './utils/errorClient';
