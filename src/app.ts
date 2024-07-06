3e98875227990d4c9548a9bc400a654cbb9cda41
Joselu <joseluissgz97@gmail.com>
2024-07-06 18:49:42 +0200

    add: Add user routes and middleware for JSON and text parsing

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,15 @@
-import express from "express";
+import express from 'express';
+import { usersRouter } from './routes/user.routes';
 
 const expressApp = express();
 
 const PORT = 3000;
 
+expressApp.use(express.json());
+expressApp.use(express.text());
+
+expressApp.use(usersRouter);
+
 expressApp.listen(PORT, () => {
-	console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
