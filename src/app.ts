6c16aaa01df6940f4ffab055132cdd0796d2cb29
Joselu <joseluissgz97@gmail.com>
2024-07-06 20:05:47 +0200

    chore: Add error handling middleware for custom errors

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
-import express from 'express';
+import express, { NextFunction, Request, Response } from 'express';
 import { usersRouter } from './routes/user.routes';
+import { ErrorClient } from './utils/errorClient';
 
 const expressApp = express();
 
@@ -9,6 +10,13 @@ expressApp.use(express.json());
 expressApp.use(express.text());
 
 expressApp.use(usersRouter);
+
+expressApp.use((err: ErrorClient, req: Request, res: Response, next: NextFunction) => {
+  return res.status(err.statusCode).json({
+    error: true,
+    message: err.message
+  });
+});
 
 expressApp.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
