d3efad7eeea1a0a110a35e15c9fdaf4a0d3de976
Joselu <joseluissgz97@gmail.com>
2024-07-06 22:57:39 +0200

    feat: Implement user login functionality with JWT authentication

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,10 +1,30 @@
 import bcrypt from 'bcrypt';
 import { Request, Response } from 'express';
+import jwt from 'jsonwebtoken';
 import { prisma } from '../config/conectDb';
+import { checkPWD } from '../helpers/checkPWD';
 import { catchError } from '../utils/catchError';
-
+import { ErrorClient } from '../utils/errorClient';
+import { responseClient } from '../utils/responseClient';
+const SECRET_KEY = process.env.SECRET_KEY;
 const userLogin = async (req: Request, res: Response) => {
-  res.send('User login');
+  const { email, password } = req.body;
+
+  const user = await prisma.user.findUnique({
+    where: {
+      email
+    }
+  });
+
+  if (!user) throw new ErrorClient('User not found', 404);
+
+  if ((await checkPWD(password, user.password.trim())) === false) throw new ErrorClient('Password incorrect', 400);
+
+  const token = jwt.sign({ id: user.user_id, name: user.name }, SECRET_KEY!, { expiresIn: 86400 });
+
+  const { password: _, ...userToSend } = user;
+
+  responseClient(res, 200, { user: userToSend, token });
 };
 
 const userRegister = async (req: Request, res: Response) => {
@@ -21,7 +41,8 @@ const userRegister = async (req: Request, res: Response) => {
       phone
     }
   });
-  res.json(newUser);
+
+  return responseClient(res, 201, newUser);
 };
 
 export default {
