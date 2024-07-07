374fd264e17c05095e9faccd236b9e3e34dfad44
Joselu <joseluissgz97@gmail.com>
2024-07-07 23:02:41 +0200

    feat: Add endpoint for retrieving user's dates

--- src/routes/date.routes.ts
+++ src/routes/date.routes.ts
@@ -1,8 +1,12 @@
 import { Router } from 'express';
 import dateController from '../controllers/date.controller';
+import { checkAuth } from '../middlewares/auth.middleware';
 import { isDateOcupied } from '../middlewares/isDateOcupied';
 
 export const dateRoute = Router();
+
+dateRoute.get('/my-dates', checkAuth, dateController.getMyDates);
+
 dateRoute.get('/date', dateController.getAllDates);
 
 dateRoute.post('/date', isDateOcupied, dateController.createNewDate);
