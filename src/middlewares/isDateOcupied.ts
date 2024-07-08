9cf9f2ab8ea44ef72b2477f95f5660e8dfc97c5c
Joselu <joseluissgz97@gmail.com>
2024-07-08 21:03:53 +0200

    feat: Update isDateOcupied middleware to handle date updates and check for date occupancy

--- src/middlewares/isDateOcupied.ts
+++ src/middlewares/isDateOcupied.ts
@@ -4,18 +4,36 @@ import { ErrorClient } from '../utils/errorClient';
 
 export const isDateOcupied = async (req: Request, res: Response, next: NextFunction) => {
   const { date_quote } = req.body;
+  const { date_id } = req.params;
   try {
-    const dateOcupied = await prisma.date.findFirst({
-      where: {
-        date_quote: new Date(date_quote)
+    //ONLY IF WE ARE CREATING A NEW DATE
+    if (!date_id) {
+      const existingDate = await prisma.date.findFirst({
+        where: { date_quote: new Date(date_quote) }
+      });
+      if (existingDate) {
+        throw new ErrorClient('Date is occupied', 400);
       }
+      return next();
+    }
+    const existingDate = await prisma.date.findUnique({
+      where: { date_id: parseInt(date_id) }
+    });
+
+    //ONLY WHEN WE ARE UPDATING A DATE
+    const isDateEquuals = existingDate?.date_quote.getTime() === new Date(date_quote).getTime();
+
+    if (isDateEquuals) {
+      return next();
+    }
+    const dateOcupied = await prisma.date.findFirst({
+      where: { date_quote: new Date(date_quote) }
     });
 
     if (dateOcupied) {
-      throw new ErrorClient('Date is occupied', 400);
+      throw new ErrorClient('Date is occupiedddddddddd', 400);
     }
-
-    next();
+    return next();
   } catch (err) {
     next(err);
   }
