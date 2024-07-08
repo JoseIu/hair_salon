766417700a13b9d50f1f9b4f369f8f5ea18732bc
Joselu <joseluissgz97@gmail.com>
2024-07-08 21:03:40 +0200

    feat: Update date route to allow updating a specific date

--- src/routes/date.routes.ts
+++ src/routes/date.routes.ts
@@ -10,3 +10,5 @@ dateRoute.get('/my-dates', checkAuth, dateController.getMyDates);
 dateRoute.get('/date', dateController.getAllDates);
 
 dateRoute.post('/date', isDateOcupied, dateController.createNewDate);
+
+dateRoute.put('/date/:date_id', checkAuth, isDateOcupied, dateController.updateDate);
